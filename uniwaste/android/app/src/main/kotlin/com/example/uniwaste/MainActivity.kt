513cdca2ce50a5feab445b32bf66b31ee1dd87c7
WeiQing <weiqing4288@gmail.com>
2025-11-28 00:37:23 +0800

    fix(android): resolve Firebase configuration and build errors

--- uniwaste/android/app/src/main/kotlin/com/example/uniwaste/MainActivity.kt
+++ uniwaste/android/app/src/main/kotlin/com/example/uniwaste/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.uniwaste
+package com.example.uniwasteApp
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
